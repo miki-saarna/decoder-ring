1694b9aeeecbc2fdaa5de69dd63503e5398824bb
Mikito Saarna <mikitosaarna@gmail.com>
2021-08-19 15:07:20 -0500

    Complete caesar.js and caesar.test.js files

--- src/caesar.js
+++ src/caesar.js
@@ -7,8 +7,24 @@ const caesarModule = (function () {
   // you can add any code you want within this function scope
 
   function caesar(input, shift, encode = true) {
-    // your solution code here
+    if(!shift || shift == 0 || shift > 25 || shift < -25) return false;
+    let inputArray = input.toLowerCase().split(''); 
+     return inputArray.map(character => {
+      if(character.match(/[a-z]/g)) { // if statement for characters that are between a-z
+        character = encode ? character.charCodeAt() + shift : character.charCodeAt() - shift; // when encoding, characters move to the right by 'shift' amount, and vice-versa for decoding
+        character = character > 122 ? character -= 26 : character; // ensuring letters wrap back around the alphabet
+        character = character < 97 ? character += 26 : character; // ensuring letters wrap back around the alphabet
+        return String.fromCharCode(character);
+      } else {
+        return character
+      }
+    })
+    .join('')  
   }
+
+// console.log(caesar("thinkful", -26))
+// console.log(String.fromCharCode(106));
+// console.log(('a').charCodeAt())
 
   return {
     caesar,
@@ -16,3 +32,5 @@ const caesarModule = (function () {
 })();
 
 module.exports = { caesar: caesarModule.caesar };
+
+// /[a-z]/i
--- test/caesar.test.js
+++ test/caesar.test.js
@@ -1,1 +1,54 @@
-// Write your tests here!
+const { expect } = require('chai');
+const { caesar } = require('../src/caesar.js');
+
+describe('caesar', () => {
+  it('should return an input with each letter shifted to the right a certain amount if encoding', () => {
+    const input = 'dragonfly';
+    const shift = 3;
+    const expected = 'gudjrqiob';
+    const actual = caesar(input, shift);
+    expect(actual).to.equal(expected);
+  })
+  it('should return an input with each letter shifted to the left a certain amount if decoding', () => {
+      const input = 'gudjrqiob';
+      const shift = 3;
+      const expected = 'dragonfly';
+      const actual = caesar(input, shift, false);
+      expect(actual).to.equal(expected);
+  })
+  it('properly wraps around the alphabet if a letter \'falls off\' beyond the alphabet while encoding', () => {
+      const input = 'xyz';
+      const shift = 3;
+      const expected = 'abc';
+      const actual = caesar(input, shift);
+      expect(actual).to.equal(expected);
+  })
+  it('properly wraps around the alphabet if a letter \'falls off\' beyond the alphabet while decoding', () => {
+    const input = 'abc';
+    const shift = 3;
+    const expected = 'xyz';
+    const actual = caesar(input, shift, false);
+    expect(actual).to.equal(expected);
+})
+it('properly reverses the direction of encoding or decoding when shift is equal to a negative integer', () => {
+    const input = 'hello world';
+    const shift = -5;
+    const expected = 'czggj rjmgy';
+    const actual = caesar(input, shift);
+    expect(actual).to.equal(expected);
+})
+it('uppercase letters convert into lowercase letters and none-alphabetical characters should be maintained as they are', () => {
+    const input = 'Hey!? What\'s up?#*$%';
+    const shift = -5;
+    const expected = 'czt!? rcvo\'n pk?#*$%';
+    const actual = caesar(input, shift);
+    expect(actual).to.equal(expected);
+})
+it('should return false if the shift value is undefined, equal to 0, greater than 25 or less than -25', () => {
+    const input = "Yellow Cat";
+    const shift = undefined;
+    const expected = false
+    const actual = caesar(input, shift);
+    expect(actual).to.equal(expected);
+})
+})
